3cdc1c025e1549dd5b7004b69163f58a9d42969d
Chao Chen <chao.chen@gatech.edu>
2014-11-26 14:12:39 -0500

    add another test case

---
+++ tests/crash2.c
@@ -0,0 +1,84 @@
+/* multi.c - test that basic persistency works for multiple segments */
+
+
+#include "rvm.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#define SEGNAME0  "testseg1"
+#define SEGNAME1  "testseg2"
+
+#define OFFSET0  10
+#define OFFSET1  100
+
+#define STRING0 "hello, world"
+#define STRING1 "black agagadrof!"
+
+
+void proc1() 
+{
+     rvm_t rvm;
+     char* segs[2];
+     trans_t trans;
+     
+     rvm = rvm_init("rvm_segments");
+
+     rvm_destroy(rvm, SEGNAME0);
+     rvm_destroy(rvm, SEGNAME1);
+
+     segs[0] = (char*) rvm_map(rvm, SEGNAME0, 1000);
+     segs[1] = (char*) rvm_map(rvm, SEGNAME1, 1000);
+
+     trans = rvm_begin_trans(rvm, 2, (void **)segs);
+
+     rvm_about_to_modify(trans, segs[0], OFFSET0, 100);
+     strcpy(segs[0]+OFFSET0, STRING0);
+     rvm_about_to_modify(trans, segs[1], OFFSET1, 100);
+     strcpy(segs[1]+OFFSET1, STRING1);
+
+	 simcrash(2);
+     rvm_commit_trans(trans);
+	
+	 rvm_unmap(rvm, segs[0]);
+	 rvm_unmap(rvm, segs[1]);
+}
+
+
+void proc2() 
+{
+     rvm_t rvm;
+     char *segs[2];
+
+     rvm = rvm_init("rvm_segments");
+	 rvm_truncate_log(rvm);
+}
+
+
+int main(int argc, char **argv) 
+{
+#if 1
+     int pid;
+
+     pid = fork();
+     if(pid < 0) {
+	  perror("fork");
+	  exit(2);
+     }
+     if(pid == 0) {
+	  proc1();
+	  exit(0);
+     }
+
+     waitpid(pid, NULL, 0);
+
+     proc2();
+#else
+	 proc1();
+	 proc2();
+#endif
+     return 0;
+}
+
